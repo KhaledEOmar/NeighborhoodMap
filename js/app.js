f0679af2c0a79bfff718789246eedd083e5e1c9f
Khaled Omar <khaledeomar@gmail.com>
2018-05-17 23:34:28 -0400

    feat: display map

--- js/app.js
+++ js/app.js
@@ -5,3 +5,16 @@ var viewModel = function(){
 var model = function(){
 
 };
+
+
+function googleMap() {
+	var testLocation = {lat: 35.777080, lng: -78.638036};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 15,
+		center: testLocation
+	});
+	var marker = new google.maps.Marker({
+		position: testLocation,
+		map: map
+	});
+};
