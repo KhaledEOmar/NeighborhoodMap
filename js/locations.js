05abd519b4e7abdf9e3a903e3d3b9d8f78734c2c
Khaled Omar <khaledeomar@gmail.com>
2018-05-20 13:50:36 -0400

    chore:moved locations to seperate file

---
+++ js/locations.js
@@ -0,0 +1,27 @@
+var locations = [
+	{
+		name:"Beasley's Chicken + Honey",
+		lat: 35.77705,
+		lng: -78.638033
+	},
+	{
+		name:"The Pit Authentic Barbecue",
+		lat: 35.7760373,
+		lng: -78.6469037		
+	},
+	{
+		name:"Bida Manda Laotian Restaurant and Bar",
+		lat: 35.7772935,
+		lng: -78.6389317		
+	},
+	{
+		name:"Glenwood Grill",
+		lat: 35.8149658,
+		lng: -78.663287		
+	},
+	{
+		name:"The Cowfish Sushi Burger Bar",
+		lat: 35.8372657,
+		lng: -78.6423387		
+	}
+];
