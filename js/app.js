f7545ac24196798b3d16639c759e5b3c25efad69
Khaled Omar <khaledeomar@gmail.com>
2018-05-22 00:01:47 -0400

    feat: added infowindow

--- js/app.js
+++ js/app.js
@@ -12,46 +12,9 @@ var places = ko.observableArray([]);
 
 var viewModel = function(){
 	var self = this;
-	this.list = ko.observableArray([]);
-};
-
-
-var Place = function(locationInfo){
-	var self = this;
-	self.visible = true;
-	self.bounce = false;
-	self.lat = locationInfo.lat;
-	self.lng = locationInfo.lng;
-	self.title = locationInfo.name;
-	self.contentString = "Hello";
+	var list = ko.observableArray([])
 	
-	self.marker = new google.maps.Marker({
-		position: new google.maps.LatLng(self.lat, self.lng),
-		animation: google.maps.Animation.DROP,
-		map: map,
-		title: self.title
-	});
 	
-	self.marker.addListener('click', function(){
-		if (self.marker.getAnimation() !== null) {
-			self.marker.setAnimation(null);
-		} else {
-			self.marker.setAnimation(google.maps.Animation.BOUNCE);
-			setTimeout(function () {
-				self.marker.setAnimation(null);
-			}, 1400);
-		}
-    });
-	
-	self.infoWindow = new google.maps.InfoWindow({
-		content: self.contentString
-	});
-	
-	self.hideMarker = function(){
-		if(self.visible){
-			self.marker.setVisible(false);
-		}
-	};
 };
 
 function initialize() {
@@ -64,6 +27,45 @@ function initialize() {
 		places.push(new Place(locations[x]));
 	}
 };
+
+var Place = function(locationInfo){
+	var self = this;
+	self.visible = true;
+	self.bounce = false;
+	self.lat = locationInfo.lat;
+	self.id = locationInfo.id;
+	self.lng = locationInfo.lng;
+	self.title = locationInfo.name;
+	self.contentString = 'Hello';
+	
+	self.marker = new google.maps.Marker({
+		position: new google.maps.LatLng(self.lat, self.lng),
+		animation: google.maps.Animation.DROP,
+		map: map,
+		title: self.title
+	});
+	
+	self.infoWindow = new google.maps.InfoWindow({
+		content: self.contentString
+	});
+	
+	self.marker.addListener('click', (function(){
+		toggleBounce(self.id);
+		self.infoWindow.open(map,self.marker);
+    }));
+
+};
+
+function toggleBounce(id){
+	for(var x = 0; x < places().length; x++){
+		places()[x].marker.setAnimation(null);
+	}
+	if (places()[id].marker.getAnimation() !== null) {
+		places()[id].marker.setAnimation(null);
+	} else {
+		places()[id].marker.setAnimation(google.maps.Animation.BOUNCE);
+	}
+}
 
 function googleError(){
 	alert("Google Maps failed to load.");
