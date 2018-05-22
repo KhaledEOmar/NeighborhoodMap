228bca7dbc6b8ad99f03914daf1b9419f971f269
Khaled Omar <khaledeomar@gmail.com>
2018-05-21 20:48:21 -0400

    feat: add marker animations

--- js/app.js
+++ js/app.js
@@ -5,31 +5,69 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+ $('.dropdown-trigger').dropdown();
 
+var map; 
+var places = ko.observableArray([]);
 
 var viewModel = function(){
+	var self = this;
+	this.list = ko.observableArray([]);
 
 };
 
-var model = function(){
 
-};
-
-
-function googleMap() {
-	var marker, x;
-	var raleigh = {lat: 35.780436, lng: -78.648135};
+var Place = function(locationInfo){
+	var self = this;
+	self.visible = true;
+	self.lat = locationInfo.lat;
+	self.lng = locationInfo.lng;
+	self.title = locationInfo.name;
+	self.contentString = "Hello";
 	
-	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 13,
-		center: raleigh
+	self.marker = new google.maps.Marker({
+		position: new google.maps.LatLng(self.lat, self.lng),
+		animation: google.maps.Animation.DROP,
+		map: map,
+		title: self.title
 	});
 	
-	for (x = 0; x < locations.length; x++){
-		marker = new google.maps.Marker({
-			position: new google.maps.LatLng(locations[x].lat, locations[x].lng),
-			map: map,
-			title: locations[x].name
-		});
+	self.marker.addListener('click', function(){
+        toggleBounce(self);
+    });
+	
+	self.infoWindow = new google.maps.InfoWindow({
+		content: self.contentString
+	});
+	
+	self.hideMarker = function(){
+		if(self.visible){
+			self.marker.setVisible(false);
+		}
+	};
+};
+
+function initialize() {
+	var raleigh = {lat: 35.780436, lng: -78.648135};
+	map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 12,
+		center: raleigh
+	});
+	for(var x =0; x<locations.length; x++){
+		places.push(new Place(locations[x]));
 	}
 };
+
+function toggleBounce(place) {
+    if (place.marker.getAnimation() !== null) {
+        place.marker.setAnimation(null);
+    } else {
+        place.marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+}
+
+function googleError(){
+	alert("Google Maps failed to load.");
+};
+
+ko.applyBindings(new viewModel());
